7f4daceae8dd0676c68beb34b070d26638f75796
YagoPazos <yagopb@gmail.com>
2024-05-02 18:58:05 +0200

    added styling to navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,33 +3,29 @@ import '../App.css'
 
 function Navbar() {
   return (
-    <nav>
-
+    <nav className='p-10 navbarstyling flex justify-between'>
         <div>
-          <h1>Hacker News</h1>
-          <div>
-            <ul>
-              <li>new</li>
-              <li>past</li>
-              <li>comments</li>
-              <li>ask</li>
-              <li>show</li>
-              <li>jobs</li>
-              <li>submit</li>
-            </ul>
-        </div>
-
+          <h1 className='p-3 font-mono'>Hacker News</h1>
+          <ul className='inline-flex p-3 text-3xl gap-4 font-mono'>
+            <a>new</a>
+            <br />
+            <a>past</a>
+            <br />
+            <a>comments</a>
+            <br />
+            <a>ask</a>
+            <br />
+            <a>show</a>
+            <br />
+            <a>jobs</a>
+            <br />
+            <a>submit</a>
+          </ul>
       </div>
-
-      
-
-        {/* Separating nav in two */}
-
-        <div>
-            <a href="">Login</a>
-            <button><input type="text" placeholder='type here what you are looking for...' /></button>
-        </div>
-
+      <div className='grid'>
+          <a className='text-5xl justify-self-end font-mono' href="">login</a>
+          <button className='search-btn-navbar py-1'><input className='text-center input-btn-navbar' type="text" placeholder='type here what you are looking for...' /></button>
+      </div>
     </nav>
   )
 }
