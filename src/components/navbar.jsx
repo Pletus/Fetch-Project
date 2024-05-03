2eca67433231ea05e9f739262749ef4377d7b5e4
YagoPazos <yagopb@gmail.com>
2024-05-03 16:02:06 +0200

    for pull

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
     <nav className='p-6 navbarstyling flex justify-between shadow-lg shadow-purple-100'>
         <div>
           <h1 className='p-3 font-mono font-bold text-black indent-4 decoration-solid'>Hacker News</h1>
-          <ul className='inline-flex p-3 text-3xl gap-4 font-mono'>
+          <ul className='inline-flex p-3 text-xl gap-4 font-mono'>
             <a className='font-bold text-black'>new</a>
             <div className='vertical-bar'></div>
             <a className='font-bold text-black'>past</a>
