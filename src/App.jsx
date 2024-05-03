47535218f70a05056bdda295adc0a49246519807
Igazi <igaziolkowska@gmail.com>
2024-05-03 16:25:01 +0200

    built OneResult component to fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
 import './App.css'
 import Navbar from './components/navbar';
+import OneResult from './components/OneResult' ;
 
 function App() {
   const [search, setSearch] = useState({});
@@ -12,6 +13,7 @@ function App() {
       <Navbar />
 
       {/* Body section */}
+      <OneResult />
 
     </>
   )
