daf7f1338c0a5b8f78d223ce4c6cb607debc03d8
Anitha <anithamca.k2008@gmail.com>
2024-05-06 18:05:44 +0200

    Search Criteria

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,15 @@ import Post from './components/post';
 
 function App() {
 
-  const [search, setSearch] = useState([]);
+  const [search, setSearch] = useState("");
   const [news, setNews] = useState({});
   const [results, setResults] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('https://hn.algolia.com/api/v1/search');
+        // const response = await fetch('https://hn.algolia.com/api/v1/search');
+        const response = await fetch(`http://hn.algolia.com/api/v1/search?query=${search}`);
         const data = await response.json();
         setResults(data.hits); // Assuming the response data is an array of results
       } catch (error) {
@@ -23,14 +24,20 @@ function App() {
     };
 
     fetchData();
-  }, []); // Empty dependency array to run only once when the component mounts
+  }, [search]); // Empty dependency array to run only once when the component mounts
+
+ 
+  const handleInputChange = (event) => {
+    setSearch(event.target.value);
+    
+  };
 
 
 
 
   return (
     <>
-      <Navbar />
+      <Navbar search={search} setSearch={setSearch} handleInputChange={handleInputChange}/>
       
 
       {/* Body section */}
